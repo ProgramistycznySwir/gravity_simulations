45c3f97983cd7cecc24b8967ba19b29eff0a6f00
Programistyczny Świr <programistycznyswir@gmail.com>
2023-02-11 18:33:41 +0100

    Simulating gravity

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use std::{ops::Range, iter::{zip}, fs::File, path::Path, fmt::Write};
 
 use indicatif::{ProgressBar, ProgressStyle, ProgressState};
 use rand::{Rng};
-use rapier3d::{prelude::*, na::Vector3};
+use rapier3d::{prelude::*, na::{Vector3, Point3}};
 
+use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 use rmp_serde::Serializer;
 use serde_derive::{Serialize, Deserialize};
 
@@ -14,27 +15,15 @@ fn main() {
         planet_spawn_area: Vector3::new(500.0, 5.0, 500.0),
         planet_mass_range: (1.0..1.0),
         planet_size_range: (1.0..1.0),
-        simulation_time: 30.0,
+        simulation_time: 300.0,
         frames_per_second: 50,
     };
 
     let mut rigid_body_set = RigidBodySet::new();
     let mut collider_set = ColliderSet::new();
 
-    /* Create the ground. */
-    // let collider = ColliderBuilder::cuboid(100.0, 0.1, 100.0).build();
-    // collider_set.insert(collider);
-
-    // /* Create the bounding ball. */
-    // let rigid_body = RigidBodyBuilder::dynamic()
-    //         .translation(vector![0.0, 10.0, 0.0])
-    //         .build();
-    // let collider = ColliderBuilder::ball(0.5).restitution(0.7).build();
-    // let ball_body_handle = rigid_body_set.insert(rigid_body);
-    // collider_set.insert_with_parent(collider, ball_body_handle, &mut rigid_body_set);
-
     let planets = random_planets(
-        params.planet_amount, 
+        params.planet_amount,
         &params.planet_spawn_area,
         &params.planet_mass_range,
         &params.planet_size_range
@@ -58,7 +47,7 @@ fn main() {
     let physics_hooks = ();
     let event_handler = ();
 
-    let time_steps = params.simulation_time as u32 * params.frames_per_second;
+    let time_steps = (params.simulation_time * params.frames_per_second as f32) as u32;
     let display_progress_bar = ProgressBar::new(time_steps.into());
     display_progress_bar
         .set_style(
@@ -67,28 +56,52 @@ fn main() {
             .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
         );
 
+    println!("Simulating {} planets for {} seconds with precision {} FPS:", params.planet_amount, params.simulation_time, params.frames_per_second);
     let mut positions: Vec<Vec<Vector3<f32>>> = Vec::new();
     /* Run the game loop, stepping the simulation once per frame. */
     for _ in 0..time_steps {
         physics_pipeline.step(
-        &gravity,
-        &integration_parameters,
-        &mut island_manager,
-        &mut broad_phase,
-        &mut narrow_phase,
-        &mut rigid_body_set,
-        &mut collider_set,
-        &mut impulse_joint_set,
-        &mut multibody_joint_set,
-        &mut ccd_solver,
-        None,
-        &physics_hooks,
-        &event_handler,
+            &gravity,
+            &integration_parameters,
+            &mut island_manager,
+            &mut broad_phase,
+            &mut narrow_phase,
+            &mut rigid_body_set,
+            &mut collider_set,
+            &mut impulse_joint_set,
+            &mut multibody_joint_set,
+            &mut ccd_solver,
+            None,
+            &physics_hooks,
+            &event_handler,
         );
 
-        for planet in &planets {
+        // let forces: Vec<Vector3<f32>> = body_handles.par_iter().map(|this_handle| {
+        let forces: Vec<Vector<f32>> = body_handles.par_iter().map(|this_handle| {
+            let this = &rigid_body_set[*this_handle];
+            let this_pos: &Point3<f32> = &this.translation().xyz().into();
+            let this_mass = &this.mass();
+            rigid_body_set.iter().map(|other_handle_and_rb| -> Vector<f32> {
+                    if &other_handle_and_rb.0 == this_handle {
+                        return Vector3::zeros();
+                    }
+                    let other = other_handle_and_rb.1;
+                    let other_pos: &Point3<f32> = &other.translation().xyz().into();
+                    let other_mass = &other.mass();
 
-        }
+                    // Gravity equation:
+
+                    let dirr = other_pos - this_pos;
+                    let dist_sq = nalgebra::distance_squared(&this_pos, &other_pos);
+
+                    let force = (this_mass * other_mass) / dist_sq;
+                    dirr.normalize() * (force * &params.gravitational_constant)
+                }).sum::<Vector3<f32>>()
+        }).collect();
+
+        zip(&body_handles, &forces).for_each(|pair| {
+            rigid_body_set[*pair.0].add_force(*pair.1, true);
+        });
 
         positions.push(
             body_handles.iter()
@@ -99,29 +112,22 @@ fn main() {
                 .collect()
             );
 
-        // let body = &rigid_body_set[body_handles[0]];
-
-        // let ball_body = &rigid_body_set[ball_body_handle];
-        // println!(
-        // "Ball altitude: {}",
-        // ball_body.translation().y
-        // );
         display_progress_bar.inc(1);
     }
-    display_progress_bar.finish_and_clear();
+    display_progress_bar.finish();
+    println!("Finished simulation!");
+    println!("Saving to file...");
 
     let data = SimulationData {
         params: params,
         planets: planets.iter().map(|planet| -> Planet {planet.planet}).collect(),
         positions: positions
     };
-    let filepath = Path::new("./sim.msgpack");
+    let filepath = Path::new("./sim_2.msgpack");
     let file = File::create(&filepath).expect("Unable to create file");
-    let mut serializer = &mut Serializer::new(file);
+    let serializer = &mut Serializer::new(file);
     serde::Serialize::serialize(&data, serializer).expect("Failed to serialize simulation data");
-    println!("Saved data to {}", filepath.display())
-    // data.serialize(serializer)
-    // f.write_all(data.as_bytes()).expect("Unable to write data");
+    println!("Saved data to {}", filepath.display());
 }
 
 
